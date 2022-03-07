66f7d3ed0fcb66c8aaad2c39c44ea8d067c0e1f1
iforgot-my-username <garibay2018paul@gmail.com>
2022-03-07 10:37:27 +0800

    Added a Color Selector

---
+++ src/ColorSelector.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import Brightness1 from "@mui/icons-material/Brightness1";
+
+interface ColorSelectorProps {
+  value?: string;
+  onChange: (event: SelectChangeEvent) => void;
+  colors: ColorModel[];
+}
+
+interface ColorModel {
+  text: string;
+  id: number;
+  color: string;
+}
+
+const ColorSelector = ({ value, onChange, colors }: ColorSelectorProps) => {
+  const menuItem = colors.map((color) => (
+    <MenuItem value={color.id}>
+      <Brightness1 style={{ color: color.color }} />
+      {color.text}
+    </MenuItem>
+  ));
+
+  return (
+    <FormControl sx={{ minWidth: 120 }}>
+      <InputLabel id="label">Color</InputLabel>
+
+      <Select
+        labelId="label"
+        value={value}
+        label="Color"
+        onChange={onChange}
+        required
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        {menuItem}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default ColorSelector;
